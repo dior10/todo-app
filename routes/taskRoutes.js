f85cb490b18bab789eaac24cdf56454242bb0442
Nicholas Calim <nicholas.uph@gmail.com>
2025-02-28 22:03:09 +0700

    Update coding

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express'); 
 const taskController = require('../controllers/taskController'); 
-const authMiddleware = require('../middleware/authMiddleware'); 
+const { authMiddleware, validateTask } = require('../middleware/authMiddleware');
 
 const router = express.Router(); 
 
 router.use(authMiddleware); 
 
-router.post('/tasks', taskController.createTask); 
+router.post('/tasks', validateTask, taskController.createTask); 
 router.get('/tasks', taskController.getTasks); 
 router.put('/tasks/:id', taskController.updateTask); 
 router.delete('/tasks/:id', taskController.deleteTask); 
